439f36ad2fc937263581f9c1298bc0279087604a
HarshR <harsh.repal2003@gmail.com>
2024-01-13 20:33:42 +0530

    Pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import Header from './components/Header'
 import Blogs from './components/Blogs'
 import { useContext, useEffect } from 'react';
 import { AppContext } from './context/AppContext';
+import { Route, Routes, useLocation, useSearchParams } from 'react-router-dom';
+import Home from './pages/Home';
+import TagPage from './pages/TagPage';
+import CategoryPage from './pages/CategoryPage';
+import BlogPage from './pages/BlogPage';
 
 function App() {
-  const {fetchBlogPosts} = useContext(AppContext)
+  const {fetchBlogPosts} = useContext(AppContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
   useEffect(()=>{
-    fetchBlogPosts();
-  },[])
+    const page = searchParams.get("page") ?? 1;
+    const tag = location.pathname.split('/').at(-1).replaceAll('-',' ');
+    const category = location.pathname.split('/').at(-1).replace('-',' ');
+ 
+    if(location.pathname.includes("tags")){
+    
+      fetchBlogPosts(Number(page),tag);
+    }
+    else if(location.pathname.includes("categories")){
+      
+      fetchBlogPosts(Number(page),null,category);
+    }
+    else{
+      fetchBlogPosts(Number(page));
+ 
+    }
+     },[location.pathname, location.search])
   return (
-    <div className="App w-full h-full flex flex-col gap-y-1 justify-center items-center">
-          <Header/>
-          <Blogs/>
-          <Pagination/>
-
-
-    </div>
+    <div>
+        
+        <Routes>
+            <Route path='/' element = {<Home/>}/>
+            <Route path='/blog/:blogId' element = {<BlogPage/>}/>
+            <Route path='/tags/:tag' element = {<TagPage/>}/>
+            <Route path='/categories/:category' element = {<CategoryPage/>}/>
+        </Routes>
+    </div>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AppContextProvider from './context/AppContext'
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AppContextProvider>
+  <BrowserRouter>
+    <AppContextProvider>
       <App />
-  </AppContextProvider>
+    </AppContextProvider>
+  </BrowserRouter>
     
 
 );
